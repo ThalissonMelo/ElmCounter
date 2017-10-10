494f933cfba5b319e6e2aba0593f1846ae12f480
Thalisson Barreto de Melo Silva <tallisonmelo@hotmail.com>
2017-10-10 08:13:43 -0300

    Add comments to counter

--- Contador.elm
+++ Contador.elm
@@ -1,27 +1,51 @@
 module Contador exposing (..)
 
-import Html exposing (div, button, text, beginnerProgram, span)
+import Html exposing (beginnerProgram, button, div, span, text)
 import Html.Events exposing (onClick)
 
-model = 0
+-- This is a model tha have the initial state of the counter
+
+
+model =
+    0
+
+
+
+-- This type have the messages of the counter
+
 
 type Msg
     = Increment
     | Decrement
 
+
+
+-- The update method recives the message when the user clicks on the button and make the
+-- action of increment or decrement
+
+
 update msg model =
     case msg of
-        Increment -> model + 1
+        Increment ->
+            model + 1
 
-        Decrement -> model - 1
+        Decrement ->
+            model - 1
+
+
+
+-- The view method shows on the screen the buttons and the result of increment and decrement
 
 view model =
     div []
-        [ 
-        button [ onClick Increment ] [text "+"]
-        , span [] [text (toString model)]
-        , button [ onClick Decrement ] [text "-"]
+        [ button [ onClick Increment ] [ text "+" ]
+        , span [] [ text (toString model) ]
+        , button [ onClick Decrement ] [ text "-" ]
         ]
+
+
+
+-- The main starts a counter showing on the screen
 
 main =
     beginnerProgram
